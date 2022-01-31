fa926cdfde9253f37e61ea7db0765170788d6c37
Jack Bruno <jzbruno@gmail.com>
2022-01-30 22:04:50 -0700

    ok let's fix the pathing again

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,14 +3,14 @@
 version="0.5.0"
 os="$(uname)"
 
-mkdir -p $HELM_PLUGINS/helm-merge-values/bin/
+mkdir -p ${HELM_PLUGIN_DIR}/bin/
 
 if [[ "${os}" == "Darwin" ]]; then
-    wget -q https://github.com/jzbruno/helm-merge-values/releases/download/${version}/helm-merge-values-darwin-amd64 -O $HELM_PLUGINS/helm-merge-values/bin/helm-merge-values
+    wget -q https://github.com/jzbruno/helm-merge-values/releases/download/${version}/helm-merge-values-darwin-amd64 -O ${HELM_PLUGIN_DIR}/bin/helm-merge-values
 elif [[ "${os}" == "Linux" ]]; then
-    wget -q https://github.com/jzbruno/helm-merge-values/releases/download/${version}/helm-merge-values-linux-amd64 -O $HELM_PLUGINS/helm-merge-values/bin/helm-merge-values
+    wget -q https://github.com/jzbruno/helm-merge-values/releases/download/${version}/helm-merge-values-linux-amd64 -O ${HELM_PLUGIN_DIR}/bin/helm-merge-values
 else
   echo "Install using 'helm plugin install' only supports Mac and Linux with amd64 architecture. Other binaries are compiled and can be found at https://github.com/jzbruno/helm-merge-values/releases/latest"
 fi
 
-chmod +x $HELM_PLUGINS/helm-merge-values/bin/helm-merge-values
+chmod +x ${HELM_PLUGIN_DIR}/bin/helm-merge-values
